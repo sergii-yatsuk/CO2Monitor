26bccd60bd2c3b983d22f6716267d0ba0f21f0c1
Denis <gritsenko.den@gmail.com>
2015-01-18 22:03:21 +0200

    Arduino firmware was updated in order to handle commands that come via LAN adapter.
    Also small refactoring was done.

--- CO2Monitor/CO2Monitor.ino
+++ CO2Monitor/CO2Monitor.ino
@@ -1,109 +1,204 @@
+#include <SPI.h>
+#include <UIPEthernet.h>
 
-/************************Hardware Related Macros************************************/
-#define         MG_PIN                       (0)     //define which analog input channel you are going to use
-#define         BOOL_PIN                     (2)
-#define         DC_GAIN                      (8.5)   //define the DC gain of amplifier
+#define cs   10
+#define dc   9
+#define rst  8
+#define rele_signal 7
+#define analogPin A0
+#define buzzer_pin 2
 
+byte mac[] = { 0x00, 0x1A, 0x79, 0x05, 0x74, 0xAA };
+EthernetServer server(3300);
+int co2 = 9999;
+float v400ppm = 3.95;   //MUST BE SET ACCORDING TO CALIBRATION
+float v40000ppm = 1.55; //MUST BE SET ACCORDING TO CALIBRATION````````````````````````
+float deltavs = v400ppm - v40000ppm;
+float A = deltavs / (log10(400) - log10(40000));
+float B = log10(400);
+int readingDelay = 500;
 
-/***********************Software Related Macros************************************/
-#define         READ_SAMPLE_INTERVAL         (50)    //define how many samples you are going to take in normal operation
-#define         READ_SAMPLE_TIMES            (5)     //define the time interval(in milisecond) between each samples in
-                                                     //normal operation
+// The IP address will be dependent on your local network.
+// If you have IP network info, uncomment the lines starting
+// with IPAddress and enter relevant data for your network.
+// If you don't know, you probably have dynamically allocated IP adresses, then
+// you don't need to do anything, move along.
+// IPAddress ip(192,168,1, 177);
+// IPAddress gateway(192,168,1, 1);
+// IPAddress subnet(255, 255, 255, 0);
 
-/**********************Application Related Macros**********************************/
-//These two values differ from sensor to sensor. user should derermine this value.
-#define         ZERO_POINT_VOLTAGE           (0.324) //define the output of the sensor in volts when the concentration of CO2 is 400PPM
-#define         REACTION_VOLTGAE             (0.020) //define the voltage drop of the sensor when move the sensor from air into 1000ppm CO2
+void printServerStatus()
+{
+  Serial.print("Server address:");
+  Serial.println(Ethernet.localIP());
+}
 
-/*****************************Globals***********************************************/
-float           CO2Curve[3]  =  {2.602,ZERO_POINT_VOLTAGE,(REACTION_VOLTGAE/(2.602-3))};
-                                                     //two points are taken from the curve.
-                                                     //with these two points, a line is formed which is
-                                                     //"approximately equivalent" to the original curve.
-                                                     //data format:{ x, y, slope}; point1: (lg400, 0.324), point2: (lg4000, 0.280)
-                                                     //slope = ( reaction voltage ) / (log400 –log1000)
+void sendResponse(EthernetClient* client, String response)
+{
+  // Send response to client.
+  client->println(response);
 
+  // Debug print.
+  Serial.println("sendResponse:");
+  Serial.println(response);
+}
+
+// Read the request line,
+String readRequest(EthernetClient* client)
+{
+  String request = "";
+
+  // Loop while the client is connected.
+  while (client->connected())
+  {
+    // Read available bytes.
+    while (client->available())
+    {
+      // Read a byte.
+      char c = client->read();
+
+      // Print the value (for debugging).
+      Serial.write(c);
+
+      // Exit loop if end of line.
+      if ('\n' == c)
+      {
+        return request;
+      }
+
+      // Add byte to request line.
+      request += c;
+    }
+  }
+  return request;
+}
+
+void executeRequest(EthernetClient* client, String* request)
+{
+  char command = readCommand(request);
+  int n = readParam(request);
+  if ('O' == command)
+  {
+    pinMode(n, OUTPUT);
+  }
+  else if ('I' == command)
+  {
+    pinMode(n, INPUT);
+  }
+  else if ('L' == command)
+  {
+    digitalWrite(n, LOW);
+  }
+  else if ('H' == command)
+  {
+    digitalWrite(n, HIGH);
+  }
+  else if ('R' == command)
+  {
+    sendResponse(client, String(digitalRead(n)));
+  }
+  else if ('A' == command)
+  {
+    sendResponse(client, String(analogRead(n)));
+  }
+}
+
+// Read the command from the request string.
+char readCommand(String* request)
+{
+  String commandString = request->substring(0, 1);
+  return commandString.charAt(0);
+}
+
+// Read the parameter from the request string.
+int readParam(String* request)
+{
+  // This handles a hex digit 0 to F (0 to 15).
+  char buffer[2];
+  buffer[0] = request->charAt(1);
+  buffer[1] = 0;
+  return (int) strtol(buffer, NULL, 16);
+}
 
 void setup()
 {
-    Serial.begin(9600);                              //UART setup, baudrate = 9600bps
-    pinMode(BOOL_PIN, INPUT);                        //set pin to input
-    digitalWrite(BOOL_PIN, HIGH);                    //turn on pullup resistors
-    pinMode(13, OUTPUT);
+  Serial.begin(9600);                              //UART setup, baudrate = 9600bps
 
-   Serial.print("MG-811 Demostration\n");
+  // Wait for serial port to connect. Needed for Leonardo only.
+  while (!Serial) {
+    ;
+  }
+
+
+  // Initialize the Ethernet shield.
+  // If you entered fixed ipaddress info, gateway, subnet mask,
+  // then uncommment the next line.
+  // Ethernet.begin(mac, ip, gateway, subnet);
+
+  // If it works to get a dynamic IP from a DHCP server, use this
+  // code to test if you're getting a dynamic adress. If this
+  // does not work, use the above method of specifying an IP-address.
+  // dhcp test starts here
+  if (Ethernet.begin(mac) == 0)
+  {
+    Serial.println("Failed to configure Ethernet using DHCP");
+    // No point in carrying on, stop here forever.
+    while (true) ;
+  }
+  // dhcp test end
+
+  // Start the server.
+  server.begin();
+
+  // Print status.
+  printServerStatus();
+
+  pinMode(analogPin, INPUT);
+  pinMode(rele_signal, OUTPUT);
+  digitalWrite(rele_signal, LOW);
+  pinMode(buzzer_pin, OUTPUT);
+  digitalWrite(buzzer_pin, HIGH);
 }
 
 void loop()
 {
-    int percentage;
-    float volts;
 
+//  int data = analogRead(analogPin); //digitise output from c02 sensor
+//  float voltage = data * 5.0 / 1024;     //convert output to voltage
+//
+//  // Calculate co2 from log10 formula (see sensor datasheet)
+//  float power = ((voltage - v400ppm) / A) + B;
+//  float co2ppm = pow(10, power);
+//  co2 = co2ppm;
+//
+//  if (co2 > 1000)
+//  {
+//    digitalWrite(buzzer_pin, LOW);
+//    delay(25);
+//    digitalWrite(buzzer_pin, HIGH);
+//  }
+//  else
+//  {
+//    digitalWrite(buzzer_pin, HIGH);
+//  }
 
-    volts = MGRead(MG_PIN);
-    Serial.print( "SEN0159:" );
-    Serial.print(volts);
-    Serial.print( "V           " );
+  // Listen for incoming client requests.
+  EthernetClient client = server.available();
+  if (!client)
+  {
+    return;
+  }
 
-    percentage = MGGetPercentage(volts,CO2Curve);
-    Serial.print("CO2:");
-    if (percentage == -1) {
-        Serial.print( "<400" );
-    } else {
-        Serial.print(percentage);
-    }
-    Serial.print( "ppm" );
-    Serial.print( "       Time point:" );
-    Serial.print(millis());
-    Serial.print("\n");
+  Serial.println("Client connected");
 
-    if (digitalRead(BOOL_PIN) ){
-        digitalWrite(13, LOW);
-        Serial.print( "=====BOOL is HIGH======" );
-    } else {
-        digitalWrite(13, HIGH);
-        Serial.print( "=====BOOL is LOW======" );
-    }
+  String request = readRequest(&client);
+  executeRequest(&client, &request);
 
-    Serial.print("\n");
+  // Close the connection.
+  client.stop();
 
-    delay(10000);
+  Serial.println("Client disonnected");
+  // wait for a moment
+  delay(readingDelay);
 }
 
-
-
-/*****************************  MGRead *********************************************
-Input:   mg_pin - analog channel
-Output:  output of SEN-000007
-Remarks: This function reads the output of SEN-000007
-************************************************************************************/
-float MGRead(int mg_pin)
-{
-    int i;
-    float v=0;
-
-    for (i=0;i<READ_SAMPLE_TIMES;i++) {
-        v += analogRead(mg_pin);
-        delay(READ_SAMPLE_INTERVAL);
-    }
-    v = (v/READ_SAMPLE_TIMES) *5/1024 ;
-    return v;
-}
-
-/*****************************  MQGetPercentage **********************************
-Input:   volts   - SEN-000007 output measured in volts
-         pcurve  - pointer to the curve of the target gas
-Output:  ppm of the target gas
-Remarks: By using the slope and a point of the line. The x(logarithmic value of ppm)
-         of the line could be derived if y(MG-811 output) is provided. As it is a
-         logarithmic coordinate, power of 10 is used to convert the result to non-logarithmic
-         value.
-************************************************************************************/
-int  MGGetPercentage(float volts, float *pcurve)
-{
-   if ((volts/DC_GAIN )>=ZERO_POINT_VOLTAGE) {
-      return -1;
-   } else {
-      return pow(10, ((volts/DC_GAIN)-pcurve[1])/pcurve[2]+pcurve[0]);
-   }
-}
---
+++ Readme.txt
@@ -0,0 +1,5 @@
+All this code is supported by Arduino 1.5.8:
+http://arduino.cc/download.php?f=/arduino-1.5.8-windows.exe
+
+The proejct uses following libraries:
+1) EtherShield - . Also add following compilation options to compiler -Wno-deprecated-declarations -D__PROG_TYPES_COMPAT__ in order to prevent errors about deprecated usage and so on.
